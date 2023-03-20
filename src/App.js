b6d2bf1808e650b65263112cdd68870621559bad
kscheltema <k87scheltema@outlook.com>
2023-03-20 11:28:12 +0200

    bug fix square bracket vs curly bracket

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 
-function Square([value, onSquareClick]) {
+function Square({ value, onSquareClick }) {
   return (
     <button className="square" onClick={onSquareClick}>
       {value}
